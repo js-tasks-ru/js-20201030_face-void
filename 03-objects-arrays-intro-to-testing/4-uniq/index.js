80f2a37afd3f4215948b381750a592c3e2685378
Александр Игнатьев <a.ignatev@inspiritum.com>
2020-11-09 09:52:38 +0300

    3 модуль 4 задача

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,14 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  let arReturn = [];
 
+  if (arr !== undefined) {
+    arr.map(e => {
+      if (!arReturn.includes(e))
+        arReturn.push(e);
+    });
+  }
+
+  return arReturn;
 }
