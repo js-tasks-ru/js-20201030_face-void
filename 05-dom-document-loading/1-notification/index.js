72c6bbec13fd2e5fab65383829b0fa06e705f746
Александр Игнатьев <a.ignatev@inspiritum.com>
2020-11-15 19:31:19 +0300

    5 модуль

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+  static existedElement = null;
 
+  constructor(mes = '',
+              {
+                duration = 1000,
+                type = 'success',
+              } = {})
+  {
+    this.duration = duration;
+    this.type = type;
+    this.message = mes;
+
+    this.render();
+  }
+
+  get template() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+              <div class="timer"></div>
+              <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">${this.message}</div>
+              </div>
+            </div>`;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.existedElement)
+      NotificationMessage.existedElement.remove();
+
+    target.append(this.element);
+    NotificationMessage.existedElement = this.element;
+
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
